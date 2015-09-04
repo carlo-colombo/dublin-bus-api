80ae2e707e27bc1a2ec0727962026c286471dc7f
Carlo Colombo <carlo.colombo@gmx.com>
2015-09-04 22:45:47 +0100

    Adding documentation

---
+++ doc/dist/sidebar_items.js
@@ -0,0 +1,10 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "Stop",
+"docs": ["get_info/1","last_time_checked/0","search/1"]
+},{"id": "Stop.Row"
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
