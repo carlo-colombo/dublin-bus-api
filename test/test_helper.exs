b15ddefc6c29e1adf93335332b12a6eea6e46a57
Carlo Colombo <carlo.colombo@gmx.com>
2016-03-01 22:40:03 +0000

    handling get with 3 params, refactoring

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,14 +2,15 @@ ExUnit.start()
 
 defmodule TestHelper do
   defmacro with_response_from_fixture(fixturefile, do: body) do
+    {:ok, fixture } = File.read(fixturefile)
     quote do
-      {:ok, fixture } = File.read(unquote(fixturefile))
-      with_mock HTTPoison, [get: fn(_url) -> {:ok,
-                                             %HTTPoison.Response{status_code: 200,
-                                                                 body: fixture}}  end,
-                             post: fn(_url,_body,_headers) -> {:ok,
-                                                              %HTTPoison.Response{status_code: 200,
-                                                                                  body: fixture}}  end], do: unquote(body)
+      resp = {:ok,
+       %HTTPoison.Response{status_code: 200,
+                           body: unquote(fixture)}}
+
+      with_mock HTTPoison, [get: fn(_url) -> resp  end,
+                           post: fn(_url,_body,_headers) -> resp end,
+                           get: fn(_,_,_) -> resp end], do: unquote(body)
     end
   end
 end
