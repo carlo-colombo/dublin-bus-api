50b5e685ca54cd5ff0a1715faa8e0698114db7df
Carlo Colombo <carlo.colombo@gmx.com>
2016-03-11 09:32:14 +0000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -11,6 +11,7 @@ defmodule DublinBusApi.Mixfile do
      description: "Access to the Real Time Passenger Information (RTPI) for Dublin Bus services.",
      name: "Dublin Bus API",
      source_url: "https://github.com/carlo-colombo/dublin-bus-api",
+     test_coverage: [tool: Coverex.Task, coveralls: true],
      package: [
        licenses: ["MIT"],
        mainteiners: ["Carlo Colombo"],
@@ -32,6 +33,7 @@ defmodule DublinBusApi.Mixfile do
      {:earmark, "~> 0.2", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev},
      {:mock, "~> 0.1.1", only: :test},
-     {:credo, "~> 0.2.5", only: :test}]
+     {:credo, "~> 0.2.5", only: :test},
+     {:coverex, "~> 1.4.7", only: :test}]
   end
 end
