97472f53944271acc3c9e53a014b7df2d943e443
Carlo Colombo <carlo.colombo@gmx.com>
2017-04-11 08:52:45 +0100

    increase elixir min version
    Using `Task.async_stream` that have been made available in 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DublinBusApi.Mixfile do
   def project do
     [app: :dublin_bus_api,
      version: "0.1.11",
-     elixir: "~> 1.0",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanentbus: Mix.env == :prod,
      deps: deps(),
